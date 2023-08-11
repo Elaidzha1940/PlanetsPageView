d4690c3576c2d8e4a10b940b7fb4583a0e64606d
Elaidzha1940 <eli080293@gmail.com>
2023-08-11 10:30:03 +0300

    Planets

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Planets
+//  File: PlanetsApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 11.08.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
