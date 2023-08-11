3acc0c5afb20d03b1288c65c2d08404ee8e988a1
Elaidzha1940 <eli080293@gmail.com>
2023-08-11 12:43:44 +0300

    Planets

--- Planets/ContentView.swift
+++ Planets/ContentView.swift
@@ -16,48 +16,51 @@ struct ContentView: View {
     
     let pages = [
         
-        OnBoardinPage(title: "Mercury", description: "", image: "Mercury"),
-        OnBoardinPage(title: "Mars", description: "", image: "Mars"),
-        OnBoardinPage(title: "Earth", description: "", image: "Earth"),
+        OnBoardinPage(title: "Mercury", description: "Mercury is the smallest planet in our solar system. It's a little bigger than Earth's Moon.", image: "Mercury"),
+        OnBoardinPage(title: "Mars", description: "​Mars is the fourth planet from the Sun – a dusty, cold, desert world with a very thin atmosphere.", image: "Mars"),
+        OnBoardinPage(title: "Earth", description: "Earth is the fifth-largest planet in the solar system. Its diameter is about 8,000 miles.", image: "Earth"),
        
     ]
     
     var body: some View {
         
         ZStack {
-            Main()
-            if isOnboardingDone{
-            } else {
-                VStack {
-                    TabView(selection: $currentPageIndex) {
-                        ForEach(0..<pages.count) { index in
-                            OnBoardinPageView(page: pages[index])
-                                .tag(index)
-                        }
-                    }
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-                    
-                    Button(action: {
-                        if currentPageIndex < pages.count - 1 {
-                            withAnimation {
-                                currentPageIndex += 1
-                            }
-                        } else {
-                            withAnimation {
-                                isOnboardingDone = true
+            ZStack {
+                Color.black
+                
+                Main()
+                if isOnboardingDone{
+                } else {
+                    VStack {
+                        TabView(selection: $currentPageIndex) {
+                            ForEach(0..<pages.count) { index in
+                                OnBoardinPageView(page: pages[index])
+                                    .tag(index)
                             }
                         }
-                    }){
-                        Text(currentPageIndex < pages.count - 1 ? "Continue" : "Start")
-                            .font(.system(size: 20, weight: .bold, design: .monospaced))
-                            .foregroundColor(.white)
-                            .padding()
+                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        
+                        Button(action: {
+                            if currentPageIndex < pages.count - 1 {
+                                withAnimation {
+                                    currentPageIndex += 1
+                                }
+                            } else {
+                                withAnimation {
+                                    isOnboardingDone = true
+                                }
+                            }
+                        }){
+                            Text(currentPageIndex < pages.count - 1 ? "Continue" : "Start")
+                                .font(.system(size: 20, weight: .bold, design: .monospaced))
+                                .foregroundColor(.white)
+                                .padding()
+                        }
                     }
-                    //.padding()
                 }
             }
         }
-        .edgesIgnoringSafeArea(.all)
+        .ignoresSafeArea()
     }
     
     struct OnBoardinPage: Identifiable {
@@ -73,12 +76,21 @@ struct ContentView: View {
         
         var body: some View {
             
-            VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading, spacing: 10) {
                 Text(page.title)
                     .font(.system(size: 40, weight: .bold, design: .monospaced))
                     .foregroundColor(.white)
+                    .cornerRadius(10)
+                
+                Text(page.description)
+                    .font(.system(size: 15, weight: .bold, design: .monospaced))
+                    .foregroundColor(.gray)
+
                 VStack {
                     Image(page.image)
+                        .resizable()
+                        .frame(maxWidth: .infinity)
+                        .aspectRatio(contentMode: .fill)
                 }
             }
             .padding()
