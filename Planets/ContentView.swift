4f49e38cd2222c2b3a3f592188e461a9928bb110
Elaidzha1940 <eli080293@gmail.com>
2023-08-11 10:49:02 +0300

    added Array

--- Planets/ContentView.swift
+++ Planets/ContentView.swift
@@ -10,11 +10,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOnboardingDone = false
+    @State private var currentPageIndex = 0
+    
+    let pages = [
+    
+        OnBoardingage(title: "", description: "", image: "Mercury"),
+        OnBoardingage(title: "", description: "", image: "Venus"),
+        OnBoardingage(title: "", description: "", image: "Earth"),
+        OnBoardingage(title: "", description: "", image: "Mars"),
+        OnBoardingage(title: "", description: "", image: "Jupiter"),
+        OnBoardingage(title: "", description: "", image: "Saturn"),
+        OnBoardingage(title: "", description: "", image: "Uranus"),
+        OnBoardingage(title: "", description: "", image: "Neptune"),
+        OnBoardingage(title: "", description: "", image: "Pluto"),
+    ]
+    
     var body: some View {
         
         VStack {
             
         }
+    }
+    
+    struct OnBoardingage: Identifiable {
+        var id = UUID()
+        var title: String
+        var description: String
+        var image: String
     }
 }
 
